ef2ebb6f54e8454954cfc1a980aa136bd2092a47
pacokwon <haechank@gmail.com>
2021-12-19 10:47:01 +0000

    feat: day6

---
+++ day6/part1/Main.hs
@@ -0,0 +1,32 @@
+import Data.List.Split (splitOn)
+import Data.Array
+import Data.Ix
+
+birthCycle :: Int
+birthCycle = 7
+
+totalDays :: Int
+totalDays = 80
+
+main :: IO ()
+main = interact $ show . solve . parse
+
+parse :: String -> [Int]
+parse = map read . splitOn ","
+
+-- https://stackoverflow.com/questions/49764615/converting-java-like-2d-dynamic-programing-matrix-to-haskell
+-- ^ about dynamic programming in haskell. helped a lot!
+memoGrowth :: Int -> Int -> Int
+memoGrowth timer days = arr ! (timer, days)
+    where
+        bounds = ((0, 0), (birthCycle + 1, totalDays))
+
+        arr = listArray bounds (map (uncurry growth) (range bounds))
+
+        growth _ 0 = 1
+        growth t d
+          | t == 0    = arr ! (birthCycle - 1, d - 1) + arr ! (birthCycle + 1, d - 1)
+          | otherwise = arr ! (t - 1, d - 1)
+
+solve :: [Int] -> Int
+solve = sum . map (flip memoGrowth totalDays)
